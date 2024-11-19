d33f173bd124ceea75dd553765f6e788a3814bd2
ceekaye-lipnica <ceekayelipnica04@gmail.com>
2024-11-19 17:39:11 +0800

    mobile view

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // TOGGLE MENU 
 const toggleMenu = document.querySelector(".toggle__menu");
-const headNav = document.querySelector(".banner-header-bot-nav");
+const headNav = document.querySelector(".banner-top-nav");
 toggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle("open");
     headNav.classList.toggle("open");
